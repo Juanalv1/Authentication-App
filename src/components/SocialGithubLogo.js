cfa4e13024bc173e44b4643d02e8906bf0e680ad
Juan <elquesififa@gmail.com>
2023-11-30 11:25:39 -0500

    fixed image compoennt

--- src/components/SocialGithubLogo.js
+++ src/components/SocialGithubLogo.js
@@ -5,7 +5,7 @@ import React from 'react'
 const SocialGithubLogo = ({name}) => {
   return (
     <div className='w-11 h-11 relative cursor-pointer' onClick={() => signIn(name, { callbackUrl: 'http://localhost:3000/profile' })}>
-      <Image src='https://i.postimg.cc/vZ9Ckf79/github.png' fill={true} alt='github'/>
+      <img src='https://i.postimg.cc/vZ9Ckf79/github.png' alt='github'/>
     </div>
   )
 }
--- src/components/SocialGoogleLogo.js
+++ src/components/SocialGoogleLogo.js
@@ -5,7 +5,7 @@ import React from 'react'
 const SocialGoogleLogo = ({name}) => {
   return (
     <div className='w-11 h-11 relative cursor-pointer' onClick={() => signIn(name, { callbackUrl: 'http://localhost:3000/profile' })}>
-      <Image src='https://i.postimg.cc/7LkjkhXR/google.png' fill={true} alt='google'/>
+      <img src='https://i.postimg.cc/7LkjkhXR/google.png' alt='google'/>
     </div>
   )
 }
